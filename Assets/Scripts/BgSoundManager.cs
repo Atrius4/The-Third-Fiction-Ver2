d0b12c33ebff78de5378f96fc6ff6eb172917d76
GrevictRodriguez <53051776+GrevictRodriguez@users.noreply.github.com>
2020-08-27 00:10:57 -0500

    Mucha Musica

---
+++ Assets/Scripts/BgSoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgSoundManager : MonoBehaviour
+{
+    public AudioSource audio1;
+    public AudioSource audio2;
+    Player_Controller player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Controller>();
+        audio2.Play();
+        audio1.Play();
+    }
+
+    void Update()
+    {
+       audio2.Pause();
+        if (player.hasDoubleJump == true)
+        {
+            audio1.Pause();
+            audio2.Play();
+
+        }
+    }
+}
+  
